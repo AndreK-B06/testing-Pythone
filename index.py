040769f244a81adea243cd1527e54a382d1ca5fa
André <137167063+AndreK-B06@users.noreply.github.com>
2024-06-19 13:52:42 +0200

    variables test and print

--- index.py
+++ index.py
@@ -3,3 +3,19 @@ print("hello world")
 # Comment test
 x = 2
 y = 8
+"""
+Comment test
+with multiple lines.
+"""
+a = str(1 + 3)
+b = int(1 + 3)
+c = float(1 + 3)
+d = 3
+e = "Tor"
+
+print(a)
+print(b)
+print(c)
+
+print(type(d))
+print(type(e))
